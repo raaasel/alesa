7258f9dbe08c9fdc2f3b1961a046ee738aea4cc5
raaasel <49531513+raaasel@users.noreply.github.com>
2021-08-14 15:18:12 +0600

    Update alesa.py

--- alesa.py
+++ alesa.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 import datetime
@@ -15,37 +14,7 @@ class bcolors:
 
 print(f"{bcolors.OKBLUE}{bcolors.BOLD}")
 t= datetime.datetime.now()
-url = 'https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getUserInfobyNickname&msisdn=8801819400400'
-myobj = {'nickname':"circle"}
-
-
-
-
-#use the 'headers' parameter to set the HTTP headers:
-x = requests.post(url, data = myobj, headers = {"x-api-key": "2c762ea43f23a61c3743b85e9371b94d","x-app-key":"000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"})
-
-#convert response to json format
-
-#parse the json to get the service ticket
-response= x.text
-#the 'demopage.asp' prints all HTTP Headers
-#8801147116455
-
-json_object = json.loads(response)
-
-json_formatted_str = json.dumps(json_object, indent=1)
-
-# print(json_formatted_str)
-# print(json_object)
-
-# Input the key value that you want to search
-keyVal = "data"
-
-# load the json data
-customer = json.loads(response)
-# Search the key value using 'in' operator
-if keyVal in customer:
-    while 1:
+while 1:
         print("Number Example 01234567890")
         name = input("Enter 1st Number ")
 
@@ -93,6 +62,3 @@ if keyVal in customer:
             print(y)
             y= y+1
         print("\nCounting Completed")
-else:
-    # Print the message if the value does not exist
-    print("SerVer Down!!!!")
